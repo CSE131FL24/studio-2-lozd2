f0c9c03178436e251854df197c8783238c106bd8
lotannaokoli6 <lot.okoli06@gmail.com>
2024-09-12 12:36:46 -0500

    pi approximaion

--- src/studio2/PiApproximation.java
+++ src/studio2/PiApproximation.java
@@ -3,7 +3,27 @@ import java.util.*;
 public class PiApproximation {
 
 	public static void main(String[] args) {
+		double xPos, yPos;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Num of iterations");
+		int iterations = scan.nextInt();
+		double numDarts = 0;
+		double totalDarts = 0;
+		double approximation;
+		for(int i = 0; i < iterations; i++) {
+			xPos = Math.random();
+			yPos = Math.random();
 
+			if((Math.pow(xPos, 2) + Math.pow(yPos, 2))<=1) {
+				numDarts +=1;
+				totalDarts +=1;
+			}else {
+				totalDarts +=1;
+			}
+		}
+		approximation = (numDarts/totalDarts)*4;
+		System.out.println(approximation);
 	}
 
 }
